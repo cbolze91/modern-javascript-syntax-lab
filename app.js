d515c7e4b5f4e8dba0cfdf44b70de406d148828b
Christina <christinamarieelaine@gmail.com>
2026-03-30 10:25:43 -0700

    exercise 5 Spread Operator on Objects

--- app.js
+++ app.js
@@ -79,3 +79,30 @@ const uncontroversialPizzaToppings = [...morePizzaToppings];
 
 console.log(uncontroversialPizzaToppings);
 
+
+// ! Exercise 5:
+// a. Duplicate the provided `anotherCar` object and spread its values into a 
+//    new variable named `myCar`.
+//
+// b. Change the `make` and `model` properties of the `myCar` object to new 
+//    values.
+//
+// c. Console log both objects and observe the results.
+//
+// Starting code (don't modify this):
+
+const anotherCar = {
+  make: 'Toyota',
+  model: 'RAV4',
+};
+
+// Your code here:
+
+const myCar = {...anotherCar};
+
+myCar.make = 'Kia';
+myCar.model = 'Seltos';
+
+console.log(anotherCar);
+console.log(myCar);
+
